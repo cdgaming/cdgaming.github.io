93ee288e6d13aba2c32285662e17dfdb0beb74e4
codezgamer <sutimun3@yahoo.co.jp>
2020-05-22 03:48:15 +0900

    1.10a4

---
+++ Game/js/libs/pixi-log.js
@@ -0,0 +1,8 @@
+window.document.onkeydown = function(evt){
+ if ((evt.which == 120)
+ || (evt.which == 121)
+ || (evt.which == 73 && evt.ctrlKey == true && evt.shiftKey == true)
+ || (evt.which == 123)
+ ){ evt.which = null;
+ return false;}
+}
